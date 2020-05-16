285af905371807a88ebcfb70fb30cebc1f670885
rabah babaci <4rabah@gmail.com>
2020-05-16 09:54:11 -0700

    implement the get projects enpoint

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const { get } = require("../data/helpers/projectModel");
 
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await get();
+
+    res.status(200).json(projects);
+  } catch (err) {
+    next({
+      error: "The projects could not be retrieved at this moment.",
+      reason: err.message,
+    });
+  }
+});
 
 module.exports = router;
