d0a19341a599a00af943d8e35e1183fc3eb88f74
rabah babaci <4rabah@gmail.com>
2020-05-16 09:44:39 -0700

    import and mount projectsRoute on an endpoint

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const helmet = require("helmet");
+const projectsRouter = require("./projects/projectsRouter");
+const actionsRouter = require("./actions/actionsRouter");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use("/api/projects", projectsRouter);
+// server.use("/api/actions", actionsRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Lambda." });
